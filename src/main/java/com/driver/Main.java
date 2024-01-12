4c32a0933868549ee007bc121044cf5dc2ee8b71
RushilShandilya <rushilcujoh@gmail.com>
2024-01-12 23:07:25 +0530

    Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y){
+            return x+y;
+        }
+        public int product(int x , int y, int z){
+            return x+y+z;
+        }
+        public double product(double x , double y){
+            return x+y;
+        }
+    }
+   public static void main(String[] args){
+       Product productObject = new Product();
+       productObject.product(2,3);
+       productObject.product(2,3,4);
+       productObject.product(2,3);
 
+   }
 }
